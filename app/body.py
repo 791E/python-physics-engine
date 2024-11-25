4ca85d43277d5de90658ff52f49590c9c2d7350c
Gian Scherer <g.a.scherer@outlook.com>
2024-11-25 11:47:04 +0100

    Split BodyAttributes into CoordinateAttributes and BodyAttributes for pylint R0902 compliance and better readability

--- app/body.py
+++ app/body.py
@@ -5,18 +5,28 @@ import pygame
 
 
 @dataclass
-class BodyAttributes:
+class CoordinateAttributes:
     """
     Attributes such as coordinates and velocity for bodies
 
     Args:
+        pos (float): Position of the body
+        vel (float): Velocity of the body
+        accel (float): Acceleration of the body
+    """
+
+    pos: float = 0
+    vel: float = 0
+    accel: float = 0
+
+
+@dataclass
+class BodyAttributes:
+    """
+    Body attributes that aren't directly related to coordinates.
+
+    Args:
         dt (float): For now arbitrary value, later pygame's tickrate should be used
-        x (float): X coordinate of the body
-        y (float): Y coordinate of the body
-        vx (float): Velocity in the x direction
-        vy (float): Velocity in the y direction
-        ax (float): Acceleration in the x direction
-        ay (float): Acceleration in the y direction
         m (float): Mass of the body
         col (tuple[int,...]): RGB or RGBA color
         bounding_box_radius (float): Half the sidelength of the smallest possible square
@@ -24,14 +34,8 @@ class BodyAttributes:
     """
 
     dt: float = 1
-    x: float = 0
-    y: float = 0
-    vx: float = 0
-    vy: float = 0
-    ax: float = 0
-    ay: float = 0
     m: float = 1
-    col: tuple[int,...] = (255, 255, 255)
+    col: tuple[int, ...] = (255, 255, 255, 255)
     bounding_box_radius: float = 1
 
 
@@ -45,33 +49,52 @@ class Body:
         update_pos: Update the body's position using the Euler-Chromer method
     """
 
-    def __init__(self, attr: BodyAttributes):
+    def __init__(
+        self,
+        x_attr: CoordinateAttributes,
+        y_attr: CoordinateAttributes,
+        attr: BodyAttributes,
+    ):
         """
         Should not be initialized on itself, but rather with 'super().__init__(attr)'
         from a subclass's __init__ constructor
 
         Args:
-            attr (BodyAttributes): Attributes that can be used by any subclass of 'Body'
+            x_attr (CoordinateAttributes): Attributes related to x coordinates
+                (e.g. position, speed)
+            y_attr (CoordinateAttributes): Attributes related to y coordinates
+                (e.g. position, speed)
+            attr (BodyAttributes): Non-coordinate attributes that can be used
+                by any subclass of 'Body'
         """
+        self.x = x_attr
+        self.y = y_attr
         self.attributes = attr
 
     def print_attrs(self) -> None:
         """Print all attributes for debugging purposes"""
+        print("\nGeneral attributes:")
         for field in fields(self.attributes):
             value = getattr(self.attributes, field.name)
             print(f"{field.name}: {value}")
 
+        print("\nx coordinate attributes:")
+        for field in fields(self.x):
+            value = getattr(self.x, field.name)
+            print(f"{field.name}: {value}")
+
+        print("\ny coordinate attributes:")
+        for field in fields(self.y):
+            value = getattr(self.y, field.name)
+            print(f"{field.name}: {value}")
+
     def update_pos(self) -> None:
         """Calculate the body's new position based on position, velocity and acceleration"""
-        self.attributes.x = self.attributes.vx * self.attributes.dt
-        self.attributes.y = self.attributes.vy * self.attributes.dt
+        self.x.pos = self.x.vel * self.attributes.dt
+        self.y.pos = self.y.vel * self.attributes.dt
 
-        self.attributes.vx = (
-            self.attributes.vx + self.attributes.ax * self.attributes.dt
-        )
-        self.attributes.vy = (
-            self.attributes.vy + self.attributes.ay * self.attributes.dt
-        )
+        self.x.vel = self.x.vel + self.x.accel * self.attributes.dt
+        self.y.vel = self.y.vel + self.y.accel * self.attributes.dt
 
 
 @dataclass
@@ -89,17 +112,24 @@ class BallAttributes:
 class Ball(Body):
     """The simplest shape for collisions: a ball"""
 
-    def __init__(self, attr: BodyAttributes, ball_attr: BallAttributes):
+    def __init__(
+        self,
+        x_attr: CoordinateAttributes,
+        y_attr: CoordinateAttributes,
+        attr: BodyAttributes,
+        ball_attr: BallAttributes,
+    ):
         """
         Args:
             attr (BodyAttributes): Attributes that could be used for any Body
             ball_attr (BallAttributes): Ball specific attributes
         """
-        super().__init__(attr)
+        super().__init__(x_attr, y_attr, attr)
         self.ball_attributes = ball_attr
 
     def print_attrs(self):
         super().print_attrs()
+        print("\nBall specific attributes:")
         for field in fields(self.ball_attributes):
             value = getattr(self.ball_attributes, field.name)
             print(f"{field.name}: {value}")
--- app/main.py
+++ app/main.py
@@ -8,15 +8,14 @@ screen: pygame.Surface = pygame.display.set_mode((1280, 720), flags=pygame.RESIZ
 clock: pygame.time.Clock = pygame.time.Clock()
 RUNNING: bool = True
 
-ball1 = body.Ball(
-    body.BodyAttributes(1, 0, 0, 10, 0, 0, 0, 1, (255, 0, 0), 1), body.BallAttributes(1)
+ball = body.Ball(
+    body.CoordinateAttributes(pos=100),
+    body.CoordinateAttributes(pos=100),
+    body.BodyAttributes(),
+    body.BallAttributes()
 )
-ball1.print_attrs()
+ball.print_attrs()
 
-ball2 = body.Ball(
-    body.BodyAttributes(1, 0, 0, 0, 0, 0, 0, 1, (0, 255, 0), 1),
-    body.BallAttributes(r=1),
-)
 
 while RUNNING:
     # Check whether the program was quit, then terminate pygame
