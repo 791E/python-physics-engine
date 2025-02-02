b326f1e8b883ca53fcc1fcb82e48ae5ade2567f9
Gian Scherer <g.a.scherer@outlook.com>
2025-02-02 02:04:10 +0100

    Fix typo in the name of temp_ball_collision

--- pysics/__init__.py
+++ pysics/__init__.py
@@ -4,7 +4,7 @@ pysics: a physics engine for the python library 'pygame'
 from .body import _Body, Ball, Polygon
 from .coordinate_system import CoordSys
 from .math_core import Vec2D
-from .temp_ball_collsion import BallCollider
+from .temp_ball_collision import BallCollider
 from .hash_map import HashMap
 
 __all__ = ["_Body", "Ball", "Polygon", "CoordSys", "Vec2D", "BallCollider", "HashMap"]
